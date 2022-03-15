0f5590fe70d6c18d9df47ab7ab0a51e0db7eb97b
hedi-sellami-shift <hedi.sellami-ext@shift-technology.com>
2022-03-15 17:28:27 +0100

    First commit

---
+++ Shift.Ocr.Abbyy.Load/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using NBomber.Contracts;
+using NBomber.CSharp;
+using NBomber.Plugins.Http.CSharp;
+
+namespace Shift.Ocr.Abbyy.Load
+{
+    class Program
+    {
+        const string baseAdress = "http://document-abbyocr-staging.corp.shift-technology.com/";
+        const string baseLocalAdress = "http://localhost:5000/";
+
+        static async Task Main(string[] args)
+        {
+            int numberRequests = 128;
+
+            Task[] tasks = new Task[numberRequests];
+            RequestHandler[] requestHandlers = new RequestHandler[numberRequests];
+
+            for (int i = 0; i < numberRequests; i++)
+            {
+                requestHandlers[i] = new RequestHandler(i.ToString());
+                var task = requestHandlers[i].SendRequest($"{baseAdress}analyze",
+                    "C:\\Users\\hedi.sellami\\Desktop\\Shift.Ocr.Abbyy.Load\\BenchmarkDocuments");
+                tasks[i] = task;
+            }
+
+            Task.WaitAll(tasks);
+
+
+            // var analyzeScenario =
+            //     CreateScenario(1, 30, 5, 
+            //         "analyze", "analyze_docs_step", "Post",
+            //         "analyze_docs_scenario");
+            // NBomberRunner
+            //     .RegisterScenarios(analyzeScenario)
+            //     .Run();
+        }
+
+
+        private static Scenario CreateScenario(int rate, int fromSeconds, int warmUpFromSeconds, string path,
+            string nameStep, string method, string nameScenario)
+        {
+            var myHttpClient = new HttpClient();
+            myHttpClient.Timeout = TimeSpan.FromMinutes(4);
+            var step = Step.Create(nameStep,
+                clientFactory: HttpClientFactory.Create("factory", myHttpClient),
+                async context =>
+                {
+                    var filesPaths = Directory.GetFiles("C:\\Users\\bensla\\Documents\\ShiftDocuments");
+                    using var content = new MultipartFormDataContent();
+
+                    foreach (var filePath in filesPaths)
+                    {
+                        var fileInfo = new FileInfo(filePath);
+                        var fileStream = System.IO.File.OpenRead(filePath);
+                        var fileContent = new StreamContent(fileStream);
+                        fileContent.Headers.Add("Content-Type", "multipart/form-data");
+                        fileContent.Headers.Add("Content-Disposition",
+                            "form-data; name=\"file\"; filename=\"" + fileInfo.Name + "\"");
+                        content.Add(fileContent, "file", fileInfo.Name);
+                    }
+
+                    var request = Http.CreateRequest(method, $"{baseAdress}{path}")
+                        .WithHeader("Language", "French")
+                        .WithBody(content)
+                        .WithCheck((response) =>
+                            Task.FromResult(response.IsSuccessStatusCode
+                                ? Response.Ok()
+                                : Response.Fail())
+                        );
+                    return await Http.Send(request, context);
+                });
+
+
+            return ScenarioBuilder
+                .CreateScenario(nameScenario, step)
+                .WithWarmUpDuration(TimeSpan.FromSeconds(warmUpFromSeconds))
+                .WithLoadSimulations(
+                    //Simulation.RampConstant(1,TimeSpan.FromSeconds(fromSeconds)));
+                    Simulation.InjectPerSec(rate: rate, TimeSpan.FromSeconds(fromSeconds)));
+        }
+    }
+}
---
+++ Shift.Ocr.Abbyy.Load/RequestHandler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Shift.Ocr.Abbyy.Load
+{
+    public class RequestHandler
+    {
+        public RequestHandler(string label = "")
+        {
+            _label = label;
+            _myHttpClient = new();
+            _content = new();
+        }
+
+        private readonly string _label;
+        private readonly HttpClient _myHttpClient;
+        private readonly MultipartFormDataContent _content;
+
+        private void BuildRequestContent(string documentsDirectoryPath)
+        {
+            var filesPaths = Directory.GetFiles(documentsDirectoryPath);
+
+            foreach (var filePath in filesPaths)
+            {
+                var fileInfo = new FileInfo(filePath);
+                var fileStream = File.OpenRead(filePath);
+                var fileContent = new StreamContent(fileStream);
+                fileContent.Headers.Add("Content-Type", "multipart/form-data");
+                fileContent.Headers.Add("Content-Disposition",
+                    "form-data; name=\"file\"; filename=\"" + fileInfo.Name + "\"");
+                _content.Add(fileContent, "file", fileInfo.Name);
+            }
+        }
+
+        public async Task SendRequest(string requestUrl, string documentsDirectoryPath)
+        {
+            BuildRequestContent(documentsDirectoryPath);
+            var timer = new Stopwatch();
+            timer.Start();
+            var task = _myHttpClient.PostAsync(requestUrl, _content);
+            Console.WriteLine($"Request {_label}: Firing");
+
+            var result = await task;
+            Console.WriteLine($"Request {_label}: status={result.StatusCode}\ttime={timer.Elapsed}");
+        }
+    }
+}
